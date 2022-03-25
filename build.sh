875324dc527ddc2120bbae9918d4cd2983e15685
FixKRI1 <97941869+FixKRI1@users.noreply.github.com>
2022-03-25 21:09:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 version="1.3.7"
 image="cpuminer-multi"
-docker build . --tag docker.io/cniweb/$image:$version
-docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:$version
-docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
-docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
-docker push ghcr.io/cniweb/$image:$version
-docker push ghcr.io/cniweb/$image:latest
-docker push docker.io/cniweb/$image:$version
-docker push docker.io/cniweb/$image:latest
+docker build . --tag docker.io/FixKRI1/$image:$version
+docker tag docker.io/FixKRI1/$image:$version ghcr.io/FixKRI1/$image:$version
+docker tag docker.io/FixKRI1/$image:$version ghcr.io/FixKRI1/$image:latest
+docker tag docker.io/FixKRI1/$image:$version docker.io/FixKRI1/$image:latest
+docker push ghcr.io/FixKRI1/$image:$version
+docker push ghcr.io/FixKRI1/$image:latest
+docker push docker.io/FixKRI1/$image:$version
+docker push docker.io/FixKRI1/$image:latest
